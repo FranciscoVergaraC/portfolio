e3e20b950d1d3b2d5e59b7ad6ff1676937959c4a
FranciscoVergaraC <franciscovergaracr@gmail.com>
2022-10-12 14:57:53 -0300

    Added hero + projects + Technologies component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br/>
+        My personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I'm an an Industrial Engenieer with +10 years of experience in the software industry 
+      </SectionText>
+      <Button onClick={()=> window.location = 'https://www.linkedin.com/in/francisco-vergara-cruz-48425743/'}>Let's connect</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
